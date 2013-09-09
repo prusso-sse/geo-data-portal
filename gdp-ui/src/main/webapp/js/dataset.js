e2d3d134acd254ce9b82ba93d9b0836e78973b7b
Carl Schroedl <cschroedl@usgs.gov>
2013-09-09 11:40:01 -0500

    Prevented ill-formed wps request by defaulting local variable to the _usingCache state variable.

--- gdp-ui/src/main/webapp/js/dataset.js
+++ gdp-ui/src/main/webapp/js/dataset.js
@@ -1065,6 +1065,7 @@ var Dataset = function() {
     }
 
     function opendapDatasetSelected(datasetURL, useCache) {
+        useCache = !!useCache; //boolean cast in case undefined
         logger.debug('GDP: Attempting to retrieve grids using OpenDAP.');
         gDatasetType = _datasetTypeEnum.OPENDAP;
         _usingCache = useCache;
@@ -1194,6 +1195,7 @@ var Dataset = function() {
 	}
 
     function getTimeRange(datasetURL, selectedGrid, useCache) {
+        useCache = !!useCache;//boolean cast in case undefined
         logger.debug('GDP: Attaining grid time range for dataset: ' + datasetURL + ' and selected grid: ' + selectedGrid);
         var getTimeRangeWpsAlgorithm = 'gov.usgs.cida.gdp.wps.algorithm.discovery.GetGridTimeRange';
         var getTimeRangeWpsInputs = {
@@ -1544,7 +1546,7 @@ var Dataset = function() {
         
         $(_SELECT_DATASET_BUTTON).click(function() {
             logger.debug('GDP: User has selected a dataset. ');
-            Dataset.datasetSelected($(_DATASET_URL_INPUT_BOX).val());
+            Dataset.datasetSelected($(_DATASET_URL_INPUT_BOX).val(), undefined ,_usingCache);
         });
 
         if (Constant.ui.default_dataset_url.length > 0) {
