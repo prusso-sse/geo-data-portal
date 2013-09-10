6bab1f6f8d4f2efb7aed5c6dadc37a07786d4b32
isuftin <isuftin@usgs.gov>
2013-09-10 12:40:33 -0500

    Killed commented code

--- gdp-derivative-ui/src/main/webapp/js/derivative_portal/components/SOS/SOSGetObservationReader.js
+++ gdp-derivative-ui/src/main/webapp/js/derivative_portal/components/SOS/SOSGetObservationReader.js
@@ -71,91 +71,9 @@ Ext.extend(GDP.SOSGetObservationReader, Ext.data.DataReader, {
             values.fois = item.fois;
             values.dataRecord = item.result.dataArray.dataRecord;
             values.values = item.result.dataArray.values;
-//            values.identifier = item.fileIdentifier.CharacterString.value;
-//            values.derivatives = [];
-//            values.scenarios = [];
-//            values.gcms = [];
-//            
-//            var idInfos = item.identificationInfo;
-//            
-//            Ext.iterate(idInfos, function (idInfo) {
-//                var keywordTypes = idInfo.descriptiveKeywords;
-//                var srvId = idInfo.serviceIdentification;
-//                var abstrakt;
-//                try {
-//                    abstrakt = Ext.decode(idInfo["abstract"].CharacterString.value, true);
-//                }
-//                catch (ex) {
-//                    // means not json, set abstract to null
-//                    abstrakt = null;
-//                }
-//                
-//                if (keywordTypes) {
-//                    Ext.iterate(keywordTypes, function (kt) {
-//                        if (kt.type.codeListValue === "derivative") {
-//                            Ext.iterate(kt.keyword, function(key) {
-//                                var derivArr = [];
-//                                var val = key.CharacterString.value;
-//                                derivArr.push(val);
-//                                if (abstrakt != null) {
-//                                    derivArr.push(abstrakt.quicktips.derivatives[val]);
-//                                }
-//                                values.derivatives.push(derivArr);
-//                            }, this);
-//                        }
-//                        else if (kt.type.codeListValue === "scenario") {
-//                            Ext.iterate(kt.keyword, function(key) {
-//                                var scenarioArr = [];
-//                                var val = key.CharacterString.value;
-//                                scenarioArr.push(val);
-//                                if (abstrakt != null) {
-//                                    scenarioArr.push(abstrakt.quicktips.scenarios[val]);
-//                                }
-//                                values.scenarios.push(scenarioArr);
-//                            }, this);
-//                        }
-//                        else if (kt.type.codeListValue === "gcm") {
-//                            Ext.iterate(kt.keyword, function(key) {
-//                                var gcmArr = [];
-//                                var val = key.CharacterString.value;
-//                                gcmArr.push(val);
-//                                if (abstrakt != null) {
-//                                    gcmArr.push(abstrakt.quicktips.gcms[val]);
-//                                }
-//                                values.gcms.push(gcmArr);
-//                            }, this);
-//                        }
-//                    }, this);
-//                }
-//                if (srvId) {
-//                    var serviceId = srvId.id;
-//                    var opMeta = srvId.operationMetadata;
-//                    if (opMeta) var linkage = opMeta.linkage;
-//                    if (linkage) var endpoint = linkage.URL;
-//                    if (serviceId) {
-//                        if (serviceId === "OPeNDAP") {
-//                            values.opendap = endpoint;
-//                        }
-//                        else if (serviceId === "OGC-WMS") {
-//                            values.wms = endpoint;
-//                        }
-//                    } 
-//                }
-//                if (abstrakt != null) {
-//                    values.fieldLabels = abstrakt.fieldlabels;
-//                }
-//                
-//            }, this);
             
             records.push(new this.recordType(values));
         }, this);
-//        values.identifier = data.records[0].fileIdentifier...;
-//        values.languages = data.languages;
-//        values.operationsMetadata = data.operationsMetadata;
-//        values.processOfferings = data.processOfferings;
-//        values.serviceIdentification = data.serviceIdentification;
-//        values.serviceProvider = data.serviceProvider;
-        //records.push(new this.recordType(values));
         
         return {
             totalRecords: records.length,
