e07e276f499be59c1eeb452f439e3bbf36f1a7ce
Suftin, Ivan <isuftin@usgs.gov>
2014-12-04 14:20:54 -0600

    Formatting

--- gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/service/BaseProcessServlet.java
+++ gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/service/BaseProcessServlet.java
@@ -10,7 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.zip.GZIPInputStream;
 import javax.naming.NamingException;
 import javax.servlet.http.HttpServlet;
@@ -29,44 +28,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
-* @author abramhall, isuftin@usgs.gov (Ivan Suftin)
-*/
+ * @author abramhall, isuftin@usgs.gov (Ivan Suftin)
+ */
 public abstract class BaseProcessServlet extends HttpServlet {
-    protected static final String WPS_NAMESPACE = "net.opengis.wps.v_1_0_0";
-    private static final Logger LOGGER = LoggerFactory.getLogger(BaseProcessServlet.class);
-    // FEFF because this is the Unicode char represented by the UTF-8 byte order mark (EF BB BF).
-    private static final String UTF8_BOM = "\uFEFF";
-    private static final int DEFAULT_OFFSET = 0;
-    private static final int DEFAULT_LIMIT = 50;
-    private static final int LIMIT_PARAM_INDEX = 1;
-    private static final int OFFSET_PARAM_INDEX = 2;
-    private static final String REQUESTS_QUERY = "select request_id from results where response_type = 'ExecuteRequest' order by request_date desc limit ? offset ?;";
-	private static final long serialVersionUID = -149568144765889031L;
-    private ConnectionHandler connectionHandler;
-	static Unmarshaller wpsUnmarshaller;
 
-    public BaseProcessServlet() {
-        String jndiName = getDatabaseProperty("jndiName");
-        if (null != jndiName) {
-            try {
-                connectionHandler = new JNDIConnectionHandler(jndiName);
-            } catch (NamingException e) {
-                LOGGER.error("Error creating database connection handler", e);
-                throw new RuntimeException("Error creating database connection handler", e);
-            }
-        } else {
-            LOGGER.error("Error creating database connection handler. No jndiName provided.");
-            throw new RuntimeException("Must configure a Postgres JNDI datasource");
-        }
-		
+	protected static final String WPS_NAMESPACE = "net.opengis.wps.v_1_0_0";
+	private static final Logger LOGGER = LoggerFactory.getLogger(BaseProcessServlet.class);
+	// FEFF because this is the Unicode char represented by the UTF-8 byte order mark (EF BB BF).
+	private static final String UTF8_BOM = "\uFEFF";
+	private static final int DEFAULT_OFFSET = 0;
+	private static final int DEFAULT_LIMIT = 50;
+	private static final int LIMIT_PARAM_INDEX = 1;
+	private static final int OFFSET_PARAM_INDEX = 2;
+	private static final String REQUESTS_QUERY = "select request_id from results where response_type = 'ExecuteRequest' order by request_date desc limit ? offset ?;";
+	private static final long serialVersionUID = -149568144765889031L;
+	static Unmarshaller wpsUnmarshaller;
+	private ConnectionHandler connectionHandler;
+
+	public BaseProcessServlet() {
+		String jndiName = getDatabaseProperty("jndiName");
+		if (null != jndiName) {
+			try {
+				connectionHandler = new JNDIConnectionHandler(jndiName);
+			} catch (NamingException e) {
+				LOGGER.error("Error creating database connection handler", e);
+				throw new RuntimeException("Error creating database connection handler", e);
+			}
+		} else {
+			LOGGER.error("Error creating database connection handler. No jndiName provided.");
+			throw new RuntimeException("Must configure a Postgres JNDI datasource");
+		}
+
 		try {
 			wpsUnmarshaller = JAXBContext.newInstance(WPS_NAMESPACE).createUnmarshaller();
 		} catch (JAXBException ex) {
-			 LOGGER.error("Error creating WPS parsing context.");
-            throw new RuntimeException("JAXBContext for " + WPS_NAMESPACE + " could not be created", ex);
+			LOGGER.error("Error creating WPS parsing context.");
+			throw new RuntimeException("JAXBContext for " + WPS_NAMESPACE + " could not be created", ex);
 		}
-    }
-    
+	}
+
 	private String getDatabaseProperty(String propertyName) {
 		Database database = WPSConfig.getInstance().getWPSConfig().getServer().getDatabase();
 		String property = null;
@@ -80,59 +80,57 @@ public abstract class BaseProcessServlet extends HttpServlet {
 		}
 		return property;
 	}
-    
-    protected final Connection getConnection() throws SQLException {
-        return connectionHandler.getConnection();
-    }
-    
-    /**
-    * @return The latest
-    * {@value gov.usgs.cida.gdp.wps.service.BaseProcessServlet#DEFAULT_LIMIT}
-    * ExecuteRequest request ids
-    * @throws SQLException
-    */
-    protected final List<String> getRequestIds() throws SQLException {
-        return getRequestIds(DEFAULT_LIMIT, DEFAULT_OFFSET);
-    }
-    
-    /**
-    *
-    * @param limit
-    * the max number of results to return
-    * @param offset
-    * which row of the query results to start returning at
-    * @return a list of ExecuteRequest request ids
-    * @throws SQLException
-    */
-    protected final List<String> getRequestIds(int limit, int offset) throws SQLException {
-        List<String> request_ids = new ArrayList<>();
-        try (Connection conn = getConnection(); PreparedStatement pst = conn.prepareStatement(REQUESTS_QUERY)) {
-            pst.setInt(LIMIT_PARAM_INDEX, limit);
-            pst.setInt(OFFSET_PARAM_INDEX, offset);
-            try (ResultSet rs = pst.executeQuery()) {
-                while (rs.next()) {
-                    String id = rs.getString(1);
-                    request_ids.add(id);
-                }
-            }
-        }
-        return request_ids;
-    }
-    
-    protected final String getIdentifier(String xml) throws JAXBException, IOException {
-        StreamSource source;
-		
+
+	protected final Connection getConnection() throws SQLException {
+		return connectionHandler.getConnection();
+	}
+
+	/**
+	 * @return The latest
+	 * {@value gov.usgs.cida.gdp.wps.service.BaseProcessServlet#DEFAULT_LIMIT}
+	 * ExecuteRequest request ids
+	 * @throws SQLException
+	 */
+	protected final List<String> getRequestIds() throws SQLException {
+		return getRequestIds(DEFAULT_LIMIT, DEFAULT_OFFSET);
+	}
+
+	/**
+	 *
+	 * @param limit the max number of results to return
+	 * @param offset which row of the query results to start returning at
+	 * @return a list of ExecuteRequest request ids
+	 * @throws SQLException
+	 */
+	protected final List<String> getRequestIds(int limit, int offset) throws SQLException {
+		List<String> request_ids = new ArrayList<>();
+		try (Connection conn = getConnection(); PreparedStatement pst = conn.prepareStatement(REQUESTS_QUERY)) {
+			pst.setInt(LIMIT_PARAM_INDEX, limit);
+			pst.setInt(OFFSET_PARAM_INDEX, offset);
+			try (ResultSet rs = pst.executeQuery()) {
+				while (rs.next()) {
+					String id = rs.getString(1);
+					request_ids.add(id);
+				}
+			}
+		}
+		return request_ids;
+	}
+
+	protected final String getIdentifier(String xml) throws JAXBException, IOException {
+		StreamSource source;
+
 		if (xml.toLowerCase().endsWith(".gz")) {
 			source = new StreamSource(new GZIPInputStream(new FileInputStream(new File(xml))));
 		} else {
 			source = new StreamSource(new StringReader(xml));
 		}
-        JAXBElement<Execute> wpsExecuteElement = wpsUnmarshaller.unmarshal(source, Execute.class);
-        Execute execute = wpsExecuteElement.getValue();
-        String identifier = execute.getIdentifier().getValue();
-        return identifier.substring(identifier.lastIndexOf(".") + 1);
-    }
-       
+		JAXBElement<Execute> wpsExecuteElement = wpsUnmarshaller.unmarshal(source, Execute.class);
+		Execute execute = wpsExecuteElement.getValue();
+		String identifier = execute.getIdentifier().getValue();
+		return identifier.substring(identifier.lastIndexOf(".") + 1);
+	}
+
 	protected final String removeUTF8BOM(String s) {
 		String result = s;
 		if (s.startsWith(UTF8_BOM)) {
