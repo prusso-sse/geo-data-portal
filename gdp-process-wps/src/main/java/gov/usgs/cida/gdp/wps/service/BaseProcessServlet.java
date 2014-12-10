dfd567786618cbca126d875b049f90c237fbc85e
Suftin, Ivan <isuftin@usgs.gov>
2014-12-10 07:58:24 -0600

    Added paging to the /list service using /list?page=x

--- gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/service/BaseProcessServlet.java
+++ gdp-process-wps/src/main/java/gov/usgs/cida/gdp/wps/service/BaseProcessServlet.java
@@ -28,15 +28,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author abramhall, isuftin@usgs.gov (Ivan Suftin)
+ * @author abramhall (Arthur Bramhall), isuftin@usgs.gov (Ivan Suftin)
  */
 public abstract class BaseProcessServlet extends HttpServlet {
 
 	protected static final String WPS_NAMESPACE = "net.opengis.wps.v_1_0_0";
 	private static final Logger LOGGER = LoggerFactory.getLogger(BaseProcessServlet.class);
 	// FEFF because this is the Unicode char represented by the UTF-8 byte order mark (EF BB BF).
-	private static final int DEFAULT_OFFSET = 0;
-	private static final int DEFAULT_LIMIT = 50;
+	protected static final int DEFAULT_OFFSET = 0;
+	protected static final int DEFAULT_LIMIT = 50;
 	private static final int LIMIT_PARAM_INDEX = 1;
 	private static final int OFFSET_PARAM_INDEX = 2;
 	private static final String REQUESTS_QUERY = "select request_id from results where response_type = 'ExecuteRequest' order by request_date desc limit ? offset ?;";
