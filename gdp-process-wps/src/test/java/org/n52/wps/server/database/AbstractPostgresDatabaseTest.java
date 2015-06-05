5e8d8bca36180f289e2bfbe1ae540ca9e095b993
mike-stern <mwstern@usgs.gov>
2015-06-05 14:47:02 -0500

    add in memory DB for postgres to enable integration testing

---
+++ gdp-process-wps/src/test/java/org/n52/wps/server/database/AbstractPostgresDatabaseTest.java
@@ -0,0 +1,99 @@
+package org.n52.wps.server.database;
+
+import static java.text.MessageFormat.format;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import ru.yandex.qatools.embed.postgresql.PostgresExecutable;
+import ru.yandex.qatools.embed.postgresql.PostgresProcess;
+import ru.yandex.qatools.embed.postgresql.PostgresStarter;
+import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
+
+public class AbstractPostgresDatabaseTest {
+
+	protected static PostgresProcess process = null;
+	protected static DataSource ds = null;
+
+	@BeforeClass
+	public static void setupInMemoryDB() throws Exception {
+		// starting Postgres
+	    PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getDefaultInstance();
+	    final PostgresConfig config = PostgresConfig.defaultWithDbName("test", "wpsTest", "abc12345");
+	    
+	    PostgresExecutable exec = runtime.prepare(config);
+	    process = exec.start();
+	
+	    // connecting to a running Postgres
+	    final String url = format("jdbc:postgresql://{0}:{1,number,#}/{2}?user={3}&password={4}",
+	            config.net().host(),
+	            config.net().port(),
+	            config.storage().dbName(),config.credentials().username(), config.credentials().password()
+	    );
+	    
+	    ds = initializeDataSource(url);
+	    
+	    setupJNDI(ds);
+	    PostgresDatabase.getInstance();
+	}
+
+	private static DataSource initializeDataSource(final String url) {
+		return new DataSource() {
+			@Override
+			public Connection getConnection(String username, String password) throws SQLException {
+				return DriverManager.getConnection(url);
+			}
+			
+			@Override
+			public Connection getConnection() throws SQLException {
+				return DriverManager.getConnection(url);
+			}
+			@Override
+			public <T> T unwrap(Class<T> iface) throws SQLException {return null;}
+			@Override
+			public boolean isWrapperFor(Class<?> iface) throws SQLException {return false;}
+			@Override
+			public void setLoginTimeout(int seconds) throws SQLException {}
+			@Override
+			public void setLogWriter(PrintWriter out) throws SQLException {}
+			@Override
+			public Logger getParentLogger() throws SQLFeatureNotSupportedException {return null;}
+			@Override
+			public int getLoginTimeout() throws SQLException {return 0;}
+			@Override
+			public PrintWriter getLogWriter() throws SQLException {return null;}
+		};
+	}
+
+	private static void setupJNDI(DataSource ds) throws NamingException {
+		// Create initial context
+	    System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+	    System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");            
+	    InitialContext ic = new InitialContext();
+	
+	    ic.createSubcontext("java:");
+	    ic.createSubcontext("java:comp");
+	    ic.createSubcontext("java:comp/env");
+	    ic.createSubcontext("java:comp/env/jdbc");
+	    
+	    ic.bind("java:comp/env/jdbc/" + "gdp", ds);
+	}
+
+	@AfterClass
+	public static void cleanupInMemoryDB() throws SQLException {
+	    process.stop();
+	}
+
+}
